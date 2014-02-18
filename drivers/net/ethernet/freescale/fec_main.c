5511dcadee751417f86eb138746b9196276ac2af
Russell King <rmk+kernel@arm.linux.org.uk>
2014-02-18 17:50:36 +0100

    NET: fec: only enable napi if we are successful
    If napi is left enabled after a failed attempt to bring the interface
    up, we BUG:
    
    fec 2188000.ethernet eth0: no PHY, assuming direct connection to switch
    libphy: PHY fixed-0:00 not found
    fec 2188000.ethernet eth0: could not attach to PHY
    ------------[ cut here ]------------
    kernel BUG at include/linux/netdevice.h:502!
    Internal error: Oops - BUG: 0 [#1] SMP ARM
    ...
    PC is at fec_enet_open+0x4d0/0x500
    LR is at __dev_open+0xa4/0xfc
    
    Only enable napi after we are past all the failure paths.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1852,8 +1852,6 @@ fec_enet_open(struct net_device *ndev)
 
 	fec_enet_clk_enable(ndev, true);
 
-	napi_enable(&fep->napi);
-
 	/* I should reset the ring buffers here, but I don't yet know
 	 * a simple way to do that.
 	 */
@@ -1869,6 +1867,7 @@ fec_enet_open(struct net_device *ndev)
 		return ret;
 	}
 
+	napi_enable(&fep->napi);
 	phy_start(fep->phy_dev);
 	netif_start_queue(ndev);
 	fep->opened = 1;
